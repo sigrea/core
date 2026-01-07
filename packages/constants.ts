639e9b7e5ad53ee60d49f483905493c3cf56741d
aose-yuu <aose.developer@gmail.com>
2026-01-08 01:02:25 +0900

    chore: allow bundler override for __DEV__

--- packages/constants.ts
+++ packages/constants.ts
@@ -1,4 +1,8 @@
+declare const __SIGREA_DEV__: boolean | undefined;
+
 export const __DEV__ =
-	typeof process !== "undefined" &&
-	typeof process.env !== "undefined" &&
-	process.env.NODE_ENV !== "production";
+	typeof __SIGREA_DEV__ !== "undefined"
+		? __SIGREA_DEV__
+		: typeof process !== "undefined" &&
+			typeof process.env !== "undefined" &&
+			process.env.NODE_ENV !== "production";
